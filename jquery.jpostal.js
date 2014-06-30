a2bb4474754377b60c9960d57496e91a63c444cb
aoki.makoto <aoki_makoto@ninton.co.jp>
2014-06-30 16:07:23 +0900

    add json data

--- jquery.jpostal.js
+++ jquery.jpostal.js
@@ -1,26 +1,73 @@
 function JpostalDatabase ( i_options ) {
-	this.address = {};	// database cache
+	this.address = [];	// database cache
+	this.map     = {
+		'_000' : true
+	};
+	this.url     = {
+		'http'  :  'http://dfufxgjxjypzf.cloudfront.net/jpostal_v2/json/',
+		'https' : 'https://dfufxgjxjypzf.cloudfront.net/jpostal_v2/json/',
+	};
+	
+	this.exists = function ( i_postcode ) {
+		var f = false;
+		
+		if ( typeof this.map['_' + i_postcode] != 'undefined' ) {
+			f = true;
+		} else {
+			f = false;
+		}
+		
+		return f;
+	};
+	
+	this.find = function ( i_postcode ) {
+		var	address;
+		
+		for ( var i = 0; i < this.address.length; ++i ) {
+			if ( this.address[i][0] == '_' + i_postcode ) {
+				address = this.address[i];
+			}
+		}
+		
+		return address;
+	};
 	
 	this.get = function ( i_postcode ) {
+		//	--------------------------------------------------
+		//	i_postcode	find()	find()	result
+		//				1234567	123
+		//	--------------------------------------------------
+		//	1			-		-		defaults
+		//	12			-		-		defaults
+		//	123			-		Y		find( '123' )
+		//	123			-		N		defaults
+		//	1234		-		Y		find( '123' )
+		//	1234		-		N		defaults
+		//	1234567		Y		-		find( '1234567' )
+		//	1234567		N		Y		find( '123' )
+		//	1234567		N		N		defaults
+		//	--------------------------------------------------
 		var defaults = ['', '', '', '', ''];
 		var	address;
+		var	head3;
 		
 		switch ( i_postcode.length ) {
 			case 3:
-				address = $.extend( defaults, this.address['_' + i_postcode]);
-				break;
-				
 			case 4:
 			case 5:
 			case 6:
-				var head3 = i_postcode.substr(0, 3);
-				address = $.extend( defaults, this.address['_' + head3]);
+				head3 = i_postcode.substr(0, 3);
+				address = this.find(head3);
+				address = $.extend( defaults, address );
 				break;
 				
 			case 7:
-				var head3 = i_postcode.substr(0, 3);
-				address = $.extend( defaults, this.address['_' + head3]);
-				address = $.extend( address , this.address['_' + i_postcode]);
+				address = this.find(i_postcode);
+				if ( !address ) {
+					head3 = i_postcode.substr(0, 3);
+					address = this.find(head3);
+				}
+				address = $.extend( defaults, address );
 				break;
 			
 			default:
@@ -31,26 +78,33 @@ function JpostalDatabase ( i_options ) {
 		return address;
 	};
 
-	this.isRequested = function ( i_postcode ) {
-		var f = false;
-		var head3 = i_postcode.substr(0, 3);
+	this.getUrl = function ( i_head3 ) {
+		var url = '';
 		
-		if ( typeof this.address['_' + head3] != 'undefined' ) {
-			f = true;
-		} else {
-			f = false;
+		switch ( window.location.protocol ) {
+			case 'http:':
+				url = this.url['http'];
+				break;
+			
+			case 'https:':
+				url = this.url['https'];
+				break;
 		}
+		url = url + i_head3 + '.json';
 		
-		return f;
+		return url;
 	};
-	
+
 	this.request = function ( i_postcode ) {
 		var _this = this;
 		var head3 = i_postcode.substr(0, 3);
 		
-		if ( i_postcode.length <= 2 || this.isRequested(head3) || head3.match(/[^0-9]/) ) {
+		if ( i_postcode.length <= 2 || this.exists(head3) || head3.match(/[^0-9]/) ) {
 			return ;
 		}
+		
+		var url = this.getUrl( head3 );
+		console.log( url );
 		
 		var options = { 
 			async         : false,
@@ -58,7 +112,7 @@ function JpostalDatabase ( i_options ) {
 			jsonp         : false,
 			jsonpCallback : '',
 			type          : 'GET',
-			url           : '//localhost/jquery.jpostal.js/jpostal_json/' + head3 + '.json',
+			url           : url,
 			error         : function(i_XMLHttpRequest, i_textStatus, i_errorThrown) {
 				;
 			},
@@ -68,8 +122,14 @@ function JpostalDatabase ( i_options ) {
 	};
 	
 	this.save = function ( i_data ) {
-		for ( var postcode in i_data ) {
-			this.address[postcode] = i_data[postcode];
+		for ( var i = 0; i < i_data.length; ++i ) {
+			var	rcd = i_data[i];
+			var postcode = rcd[0];
+			
+			if ( typeof this.map[postcode] == 'undefined' ) {
+				this.address.push( rcd );
+				this.map[postcode] = true;
+			}
 		}
 	};
 }
@@ -90,9 +150,9 @@ function Jpostal ( i_JposDb ) {
 	this.formatAddress = function ( i_fmt, i_address ) {
 		var	s = i_fmt;
 		
-		s = s.replace( /%3|%p|%prefecture/, i_address[0] );
-		s = s.replace( /%4|%c|%city/      , i_address[1] );
-		s = s.replace( /%5|%t|%town/      , i_address[2] );
+		s = s.replace( /%3|%p|%prefecture/, i_address[1] );
+		s = s.replace( /%4|%c|%city/      , i_address[2] );
+		s = s.replace( /%5|%t|%town/      , i_address[3] );
 		
 		return s;
 	};
@@ -114,6 +174,10 @@ function Jpostal ( i_JposDb ) {
 		}
 		
 		this.options.address = i_options.address;
+		
+		if ( typeof i_options.url != 'undefined' ) {
+			this.jposDb.url = i_options.url;
+		}
 	};
 	
 	this.main = function () {
